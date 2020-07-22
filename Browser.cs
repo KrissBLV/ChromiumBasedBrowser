dcd735b58c2d85a071ddbf314abb30abd28c5fec
kristofers birks <kristofers.birks@gmail.com>
2020-07-22 17:10:55 +0300

    5.10

---
+++ Browser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace ChromiumBasedBrowser
+{
+    public partial class Browser : Form
+    {
+
+        public ChromiumWebBrowser browser;
+        public Browser()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+        }
+
+        private void InitializeBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https:/google.com");
+            browser.Width = this.Width;
+            browser.Height = this.Height;
+            this.Controls.Add(browser);
+        }
+    }
+}
